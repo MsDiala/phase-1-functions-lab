5b7b57559a7707ecb88b44d00cd8026381b7c729
MsDiala <diala-is-diala@hotmail.com>
2023-10-15 21:18:34 +0100

    done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 42;
+    return Math.abs(pickupLocation - hqLocation);
+  }
+  
+  function distanceFromHqInFeet(pickupLocation) {
+    const blocks = distanceFromHqInBlocks(pickupLocation);
+    return blocks * 264;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  
+//   console.log(distanceFromHqInBlocks(50)); // Should return 8
+//   console.log(distanceFromHqInFeet(50));   // Should return 2112
+//   console.log(distanceTravelledInFeet(34, 38));  // Should return 1056
+//   console.log(calculatesFarePrice(34, 38));     // Should return 8.32
+  
